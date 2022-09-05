267b84439fc022e8f33f70569aab1014d602ef51
Alexandre-Arauj <alexandrearaujo2299@gmail.com>
2022-09-05 11:08:04 -0300

    Refresh Token
    Criando a função para utilizar o refresh token

--- src/config/jwt.mjs
+++ src/config/jwt.mjs
@@ -9,7 +9,11 @@ export const authentication = (userId) => {
         expiresIn: 300
     })
 
-    return jwt
+    const refreshToken = jsonwebtoken.sign({id: userId + (Math.random() * 100)}, PRIVATE_KEY, {
+        expiresIn: 300
+    })
+
+    return [jwt, refreshToken]
 }
 
 export const verifyToken = (req, res, next) => {
@@ -19,6 +23,19 @@ export const verifyToken = (req, res, next) => {
         if(err) return res.status(500).send({auth: false, message: 'Falha ao autenticar o token'})
 
         req.userId = decoded.id
+        next();
+    })
+}
+
+export const refreshToken = (refreshToken) => {
+    if(!refreshToken) return res.status(401).send({auth: false, message: 'Não foi fornecido um token de acesso.'})
+    jsonwebtoken.verify(refreshToken, PRIVATE_KEY, function(err, decoded) {
+        if(err) return res.status(500).send({auth: false, message: 'Falha ao autenticar o token'})
+        const jwt = jsonwebtoken.sign({id: Math.random() * 1000}, PRIVATE_KEY, {
+            expiresIn: 300
+        })
+
+        return res.status(200)
         next();
     })
 }
