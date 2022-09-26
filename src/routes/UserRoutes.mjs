c4c8977300cf8f1be3063435fa5df5f3dd9c8b86
Vinicius Gabriel Tronco Fausto <viniciusgtfausto@gmail.com>
2022-09-25 23:14:57 -0300

    rota e controller criados para a inserção de um novo usuário

--- src/routes/UserRoutes.mjs
+++ src/routes/UserRoutes.mjs
@@ -1,5 +1,9 @@
 import express from "express";
-
+import {createUser} from "../controller/UserController.mjs";
 const router = express.Router();
 
+
+router.post('/user', createUser);
+
 export const userRoutes = router
+
