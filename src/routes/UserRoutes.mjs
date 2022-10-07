cf18c225c79da08f9537fa34bd4be820f26da232
Vinicius Gabriel Tronco Fausto <viniciusgtfausto@gmail.com>
2022-10-07 13:54:18 -0300

    feito um novo endpoint para o usuário atualizar a senha

--- src/routes/UserRoutes.mjs
+++ src/routes/UserRoutes.mjs
@@ -1,12 +1,13 @@
 import express from "express";
 import { verifyToken } from "../config/jwt.mjs";
-import {createUser, inactivatedUser, updateUser, getUserById, getAllUsers, login, getUsersWithTheirsSettingsById} from "../controller/UserController.mjs";
+import {createUser, inactivatedUser, updateUser, getUserById, getAllUsers, login, getUsersWithTheirsSettingsById, updateUserPassword} from "../controller/UserController.mjs";
 const router = express.Router();
 
 
 router.post('/', createUser);
 router.patch('/:id', verifyToken, inactivatedUser);
 router.put('/:id', verifyToken, updateUser);
+router.patch('/', verifyToken, updateUserPassword);
 router.get('/getAll', verifyToken, getAllUsers);
 router.get('/:id', verifyToken, getUserById);
 router.get('/getSettings/:id', verifyToken, getUsersWithTheirsSettingsById);
